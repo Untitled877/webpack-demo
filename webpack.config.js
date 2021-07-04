667cd00a304b8f5c09261c5285f29bf51bb94b64
Sun <1113425327@qq.com>
2021-07-04 23:58:02 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,22 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  ...base,
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist"
   },
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.[contenthash].js',
-  },
-  plugins: [new HtmlWebpackPlugin({
-    title: '写代码啦',
-    template: 'src/assets/index.html'
-  }),
-  new MiniCssExtractPlugin({
-    filename: "[name].[contenthash].css",
-    chunkFilename: "[id].[contenthash].css",
-    ignoreOrder: false, 
-  }),
-],
   module: {
     rules: [
+      ...base.module.rules,
       {
         test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../",
-            },
-          },
-          "css-loader",
-        ],
-        // use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
